638a9fa5522bc200550e4f4d6c4239a73e28dd23
sangha319 <dnjs7848@gmail.com>
2025-11-14 10:50:20 +0900

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
-document.getElementById("open-chat").addEventListener("click", function() {
-  document.getElementById("start-screen").style.display = "none";
-  document.getElementById("chat-screen").classList.remove("hidden");
+dBtn.addEventListener('click', () => {
+  dateScreen.classList.add('hidden');
+  
+  // 기존에 보여진 채팅 숨기기
+  document.querySelectorAll('.chat-container').forEach(c => c.classList.add('hidden'));
+  
+  // 해당 날짜 채팅 보여주기
+  document.getElementById(`chat-${year}-${date.replace(/[^0-9]/g,'-')}`).classList.remove('hidden');
+  
+  chatScreen.classList.remove('hidden');
 });
