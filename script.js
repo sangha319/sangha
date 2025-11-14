653eeeaa16386e3ffc54d56be43efe4dbc25aa82
sangha319 <dnjs7848@gmail.com>
2025-11-14 10:35:39 +0900

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("open-chat").addEventListener("click", function() {
+  document.getElementById("start-screen").style.display = "none";
+  document.getElementById("chat-screen").classList.remove("hidden");
+});
